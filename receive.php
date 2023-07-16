c66f3432010a18f56b004c0ab3b09d01dad25146
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-16 17:45:22 +0900

    print_r関数を見やすくする

--- receive.php
+++ receive.php
@@ -5,8 +5,9 @@
 <title>出力結果</title>
 </head>
 <body>
+<pre>
 <?php
-//print_r($_POST);
+print_r($_POST);
 echo htmlspecialchars($_POST['recipe_name'],ENT_QUOTES,'UTF-8');
 echo "<br>";
 if ($_POST['category'] === '1') echo "和食";
@@ -14,5 +15,6 @@ if ($_POST['category'] === '2') echo "中華";
 if ($_POST['category'] === '3') echo "洋食";
 echo "<br>";
 ?>
+</pre>
 </body>
 </html>
