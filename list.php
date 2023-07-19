ce1dd96e1bca19ce2c8c1f804cc92a098f4e4089
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-19 17:30:56 +0900

    一覧表示の見出し

--- list.php
+++ list.php
@@ -7,7 +7,11 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "SELECT * FROM recipes";
 $stmt = $dbh->query($sql);
 $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
-print_r($result);
+echo "<table>\n";
+echo "<tr>\n";
+echo "<th>料理名</th><th>予算</th><th>難易度</th>\n";
+echo "</tr>\n";
+echo "</table>\n";
 $dbh = null;
 } catch (Exception $e) {
 echo "エラー発生: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . "<br>";
