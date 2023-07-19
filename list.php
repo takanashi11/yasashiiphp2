e92ea7611b4a5284e950432f4fd56fcb2e44b35e
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-19 17:12:03 +0900

    データベースのエラーチェック

--- list.php
+++ list.php
@@ -1,6 +1,7 @@
 <?php
 $user ="suzuki";
 $pass ="Pw111";
+try {
 $dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8',$user, $pass);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "SELECT * FROM recipes";
@@ -8,4 +9,8 @@ $stmt = $dbh->query($sql);
 $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
 print_r($result);
 $dbh = null;
+} catch (Exception $e) {
+echo "エラー発生: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . "<br>";
+die();
+}
 ?>
