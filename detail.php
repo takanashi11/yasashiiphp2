5bd6be4a7e1e20d499282c8b18c21245e3d026c7
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-21 21:08:24 +0900

    プレースホルダの設定

--- detail.php
+++ detail.php
@@ -4,7 +4,16 @@ $pass ="Pw111";
 try {
 if (empty($_GET['id'])) throw new Exception('ID不正');
 $id = (int) $_GET['id'];
-var_dump($id);
+$dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
+$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "SELECT * FROM recipes WHERE id = ?";
+$stmt = $dbh->prepare($sql);
+$stmt->bindValue(1, $id, PDO::PARAM_INT);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+print_r($result);
+$dbh = null;
 } catch (Exception $e) {
 echo "エラー発生: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . "<br>";
 die();
