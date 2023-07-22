d92df8a0d4cdf1b99d63ad4dc99515e89fea3179
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-22 13:00:35 +0900

    入力内容の型の変換

---
+++ add.php
@@ -0,0 +1,8 @@
+<?php
+$user ="suzuki";
+$pass ="Pw111";
+$recipe_name = $_POST['recipe_name'];
+$howto = $_POST['howto'];
+$category = (int) $_POST['category'];
+$difficulty = (int) $_POST['difficulty'];
+$budget = (int) $_POST['budget'];
