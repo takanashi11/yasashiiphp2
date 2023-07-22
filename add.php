fc1bb11eb7bdb8914583c30b83a0ba73e43fc6d8
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-22 13:27:20 +0900

    データベースに内容を保存する

--- add.php
+++ add.php
@@ -6,3 +6,21 @@ $howto = $_POST['howto'];
 $category = (int) $_POST['category'];
 $difficulty = (int) $_POST['difficulty'];
 $budget = (int) $_POST['budget'];
+try {
+$dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
+$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "INSERT INTO recipes (recipe_name, category, difficulty, budget, howto) VALUES (?, ?, ?, ?, ?)";
+$stmt = $dbh->prepare($sql);
+$stmt->bindValue(1, $recipe_name, PDO::PARAM_INT);
+$stmt->bindValue(2, $category, PDO::PARAM_INT);
+$stmt->bindValue(3, $difficulty, PDO::PARAM_INT);
+$stmt->bindValue(4, $budget, PDO::PARAM_INT);
+$stmt->bindValue(5, $howto, PDO::PARAM_INT);
+$stmt->execute();
+$dbh = null;
+echo "レシピの登録が完了しました。";
+} catch (Exception $e) {
+echo "エラー発生: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . "<br>";
+die();
+}
